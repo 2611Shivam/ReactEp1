7bc699dfe53249607517508befa4cebad3faf720
kumar shivam <123308055+2611Shivam@users.noreply.github.com>
2023-07-01 12:23:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+    <User />
+    </div>
+  );
+}
+
+const User=()=>{
+  return(
+    <div>
+      <h1>shivam</h1>
+      <h1> 22 </h1>
+      <h1> kshivam7992@gmail.com </h1> 
+    </div>
+  )
+}
